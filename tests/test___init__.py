bccfa2a41c30892b3be785eef51be9e5145980ad
Will Riley <wanderingwill@gmail.com>
2025-04-28 13:02:42 +0200

    Update test___init__.py
    Added test.

--- tests/test___init__.py
+++ tests/test___init__.py
@@ -12,3 +12,8 @@ def test_parse():
     yarnlock = Path('tests/yarn.lock').read_text()
     yarnlock_expected = json.loads(Path('tests/yarn.lock.json').read_bytes())
     assert yarnlock_parse(yarnlock) == yarnlock_expected
+
+def test_parse_same_package_name_with_different_semantic_versions_resolve_to_different_exact_versions():
+    yarnlock = Path('tests/same_package_name_with_different_semantic_versions_resolve_to_different_exact_versions/yarn.lock').read_text()
+    yarnlock_expected = json.loads(Path('tests/same_package_name_with_different_semantic_versions_resolve_to_different_exact_versions/yarn.lock.json').read_bytes())
+    assert yarnlock_parse(yarnlock) == yarnlock_expected
